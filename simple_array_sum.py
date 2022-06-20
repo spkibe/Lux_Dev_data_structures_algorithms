125dda491fc58308282dfc48bdcb1558fda2b68d
sp kibet <spkibet@gmail.com>
2022-06-20 22:16:16 +0300

    simple array sum challenge

---
+++ simple_array_sum.py
@@ -0,0 +1,18 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'simpleArraySum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY ar as parameter.
+#
+
+def simpleArraySum(ar):
+    return sum([i for i in ar])
+
+    
+print(simpleArraySum([1, 2, 3, 4, 10, 11]))
